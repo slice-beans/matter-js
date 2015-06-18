c1c49e89b04e0c957972b929fe41e14337dd766a
Slice <matt.gray@retrofuzz.com>
2015-06-18 12:38:06 +0000

    Removed incorrect reliance on intertia for calculating torque

--- build/matter.js
+++ build/matter.js
@@ -1,5 +1,5 @@
 /**
-* matter.js edge-master 2015-06-17
+* matter.js edge-master 2015-06-18
 * http://brm.io/matter-js/
 * License: MIT
 */
@@ -609,7 +609,7 @@ var Body = {};
         body.force.x += force.x;
         body.force.y += force.y;
         var offset = { x: position.x - body.position.x, y: position.y - body.position.y };
-        body.torque += (offset.x * force.y - offset.y * force.x) * body.inverseInertia;
+        body.torque += (offset.x * force.y - offset.y * force.x) ;
     };
 
     /**
--- src/body/Body.js
+++ src/body/Body.js
@@ -567,7 +567,7 @@ var Body = {};
         body.force.x += force.x;
         body.force.y += force.y;
         var offset = { x: position.x - body.position.x, y: position.y - body.position.y };
-        body.torque += (offset.x * force.y - offset.y * force.x) * body.inverseInertia;
+        body.torque += (offset.x * force.y - offset.y * force.x) ;
     };
 
     /**
